9009ef9ceeab8fe5c7f390fc2ba1e2be3b172e7d
Jacob Gayban <eanj13@gmail.com>
2022-04-07 16:55:48 -0700

    Using baseUrl for Cypress

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -1,12 +1,12 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('/')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('check ivysaur page', function() {
-    cy.visit('http://localhost:5000/pokemon/ivysaur')
+    cy.visit('/pokemon/ivysaur')
     cy.contains('chlorophyll')
   })
 })
