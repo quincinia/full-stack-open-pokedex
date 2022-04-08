1c5ddc4e30555997b65a00cb98de8a324a3314ff
Jacob Gayban <eanj13@gmail.com>
2022-04-07 17:04:46 -0700

    Fix Cypress broken navigation

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,15 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+// https://github.com/cypress-io/cypress/issues/3245#issuecomment-458899764
+module.exports = (on) => {
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') { 
+      // ^ make sure this is your browser name, you may 
+      // be using 'canary' or 'chromium' for example, so change it to match!
+      args.push('--proxy-bypass-list=<-loopback>')
+      return args
+    }
+  })
 }
